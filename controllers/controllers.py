8cb362a3153abffd1ed3f15e3f2a9abcedc092b7
gabriel <gabosoft.ape@outlook.com>
2021-05-25 23:10:07 -0500

    test controlller

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Todoapp-owl(http.Controller):
-#     @http.route('/todoapp-owl/todoapp-owl/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class TodoappOwl(http.Controller):
+    #     @http.route('/todoapp-owl/todoapp-owl/', auth='public')
+    #     def index(self, **kw):
+    #         return "Hello, world"
 
-#     @http.route('/todoapp-owl/todoapp-owl/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('todoapp-owl.listing', {
-#             'root': '/todoapp-owl/todoapp-owl',
-#             'objects': http.request.env['todoapp-owl.todoapp-owl'].search([]),
-#         })
+    #     @http.route('/todoapp-owl/todoapp-owl/objects/', auth='public')
+    #     def list(self, **kw):
+    #         return http.request.render('todoapp-owl.listing', {
+    #             'root': '/todoapp-owl/todoapp-owl',
+    #             'objects': http.request.env['todoapp-owl.todoapp-owl'].search([]),
+    #         })
 
-#     @http.route('/todoapp-owl/todoapp-owl/objects/<model("todoapp-owl.todoapp-owl"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('todoapp-owl.object', {
-#             'object': obj
-#         })
+    @http.route('/todoapp/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('todoapp-owl.home', {
+            'home': obj
+        })
